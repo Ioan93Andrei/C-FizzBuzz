36d120a5e21412701fe2edb9952a7849fde806bd
Ioan Andrei <ivan.andrei93@mail.ru>
2023-06-29 23:56:37 +0200

    A small finished FizzBuzz challenge in C#

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿for (int i = 1; i < 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+        Thread.Sleep(1000);
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+        Thread.Sleep(1000);
+    }
+    else if(i % 5 == 0 )
+    {
+        Console.WriteLine("Buzz");
+        Thread.Sleep(1000);
+    }
+    else
+    {
+        Console.WriteLine(i);
+        Thread.Sleep(1000);
+    }
+}

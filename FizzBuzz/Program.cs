16372b55b346173f9442821e3dbc65b0b59dd7f6
Ioan Andrei <ivan.andrei93@mail.ru>
2023-06-30 00:06:22 +0200

    Moved Thread.Sleep outside the loop to avoid repetition

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -3,21 +3,18 @@
     if (i % 3 == 0 && i % 5 == 0)
     {
         Console.WriteLine("FizzBuzz");
-        Thread.Sleep(1000);
     }
     else if (i % 3 == 0)
     {
         Console.WriteLine("Fizz");
-        Thread.Sleep(1000);
     }
     else if(i % 5 == 0 )
     {
         Console.WriteLine("Buzz");
-        Thread.Sleep(1000);
     }
     else
     {
         Console.WriteLine(i);
-        Thread.Sleep(1000);
     }
+    Thread.Sleep(1000);
 }
